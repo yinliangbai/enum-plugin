43165346b51a99b8a76772cb461066328b809f81
GitHub Action <action@github.com>
2022-06-10 14:18:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/yinliangbai/enumplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yinliangbai/enumplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yinliangbai.enumplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yinliangbai.enumplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/yinliangbai/enumplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yinliangbai/enumplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yinliangbai.enumplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yinliangbai.enumplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
